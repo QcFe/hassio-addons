821a7150a125dad62fea8855d56d25efa92560a6
qcfe <io@qcfe.me>
2021-02-11 19:37:51 +0100

    fix: enforcing version & config schema

--- nextcloud/rootfs/usr/bin/run.sh
+++ nextcloud/rootfs/usr/bin/run.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
 
+alias occ="php /share/nextcloud/html/occ"
+
 SHARE_DIR=/share/nextcloud
 if [ ! -d "${SHARE_DIR}" ]; then
     mkdir -p "${SHARE_DIR}"/html/data
@@ -10,7 +12,7 @@ fi
 
 CONFIG_PATH=/data/options.json
 echo 'Starting with the following configuration:';
-jq --raw-output 'keys[] as $k | select(.[$k] != "" and .[$k] != null) | "\t" + ($k | ascii_upcase) + "=\"" + (.[$k]|tostring) + "\""' $CONFIG_PATH;
+jq --raw-output 'keys[] as $k | select(.[$k] != "" and .[$k] != null) | "\t" + ($k | ascii_upcase) + "=\"" + (.[$k]|tostring) + "\"\n"' $CONFIG_PATH;
 eval $(jq --raw-output 'keys[] as $k | select(.[$k] != "" and .[$k] != null) | "export " + ($k | ascii_upcase) + "=\"" + (.[$k]|tostring) + "\""' $CONFIG_PATH);
 
 mount -t cifs -o ${SMB_OPTS} -v ${SMB_SHARE} ${SHARE_DIR}/html/data
